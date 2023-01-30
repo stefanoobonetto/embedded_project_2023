e1bcb4f72e415e0bac9ee4770f40b54b295f9248
simone <83060406+Sro552@users.noreply.github.com>
2023-01-30 15:54:19 +0100

    Add files via upload

---
+++ MSP432_RECEIVER/UltrasonicSensor.h
@@ -0,0 +1,16 @@
+/*
+ * UltrasonicSensor.h
+ *
+ *  Created on: 30 gen 2023
+ *      Author: simoneroman
+ */
+
+#ifndef ULTRASONICSENSOR_H_
+#define ULTRASONICSENSOR_H_
+
+void configUltraSonic();
+
+uint32_t leggiDati();
+
+
+#endif /* ULTRASONICSENSOR_H_ */
