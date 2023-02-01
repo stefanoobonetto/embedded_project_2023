85592e14c6f8dd0651e790844bdb2091b03bc33c
simone <83060406+Sro552@users.noreply.github.com>
2023-02-01 17:59:41 +0100

    Update ESP32_SENDER.ino

--- ESP32_SENDER/ESP32_SENDER.ino
+++ ESP32_SENDER/ESP32_SENDER.ino
@@ -3,49 +3,40 @@
 #include <WiFi.h>
 #include <HardwareSerial.h>
 
-
-
 HardwareSerial SerialPort(2); // use UART2
 
 // MAC Address of responder - edit as required
 uint8_t broadcastAddress[] = {0xC0, 0x49, 0xEF, 0xCD, 0x16, 0xE8};
 
-// Define a data structure
+// Define a data structure for WIFI
 typedef struct struct_message {
   uint8_t a;
 } struct_message;
 
-// Create a structured object
 struct_message myData;
 
 // Peer info
 esp_now_peer_info_t peerInfo;
 
-void setup() {
-  
-  // Set up Serial Monitor
-  Serial.begin(115200);
- 
-  // Set ESP32 as a Wi-Fi Station
+void initWIFI(){
+   // Set ESP32 as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-
   // Initilize ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
+  esp_now_init();
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
-  
   // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
+  esp_now_add_peer(&peerInfo);
+}
 
+void setup() {
+  
+  // Set up Serial Monitor
+  Serial.begin(115200);
+  // Init wifi
+  initWIFI();
   //SET UART
   SerialPort.begin(115200, SERIAL_8N1, 17, 16); //17 rx 16 tx
 }
